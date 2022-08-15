264b843cc54adc268a43ac49873cff59c8f2ff4c
ElCosmoXD <benjaminsalazarballadares@gmail.com>
2022-08-15 13:03:04 -0400

    Change the name of the 'Main' func to 'Setup'

---
+++ DDLCPlusInjector/Injector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DDLCPlusInjector
+{
+    public class Injector
+    {
+        private static List<string> dll_files = new List<string>();
+
+        public static void Setup()
+        {
+            dll_files.AddRange(Directory.GetFiles("mods/", "*.dll", SearchOption.AllDirectories));
+
+            foreach(string dll in dll_files)
+            {
+                UnityEngine.Debug.Log("Importing dll " + dll);
+            }
+        }
+    }
+}
