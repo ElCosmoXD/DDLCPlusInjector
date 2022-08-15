87e7274517af600c3be2e547ba403aa051ee9621
ElCosmoXD <benjaminsalazarballadares@gmail.com>
2022-08-15 16:18:02 -0400

    Add the first setup to the patch

---
+++ Patch/LauncherMain.cs
@@ -0,0 +1,39 @@
+//...
+using System.Reflections;
+
+namespace RenpyLauncher
+{
+	public class LauncherMain : MonoBehaviour
+	{
+		//....
+		public void OnLauncherLoaded()
+		{
+			//...
+			try    
+			{    
+			 	Debug.Log("Loading the injector assembly...");     
+				Assembly assembly = Assembly.LoadFrom("DDLCPlusInjector.dll");     
+				
+				if (assembly == null)     
+					Debug.LogError("Can't find assembly!");     
+				
+				Debug.Log("Searching for the 'DDLCPlusInjector.Injector' class and calling the 'Setup' function!");     
+				Type type = assembly.GetType("DDLCPlusInjector.Injector", true);     
+				if (type == null)          
+					 Debug.LogError("Can't find 'DDLCPlusInjector.Injector' type!");     
+
+				MethodInfo method = type.GetMethod("Setup", BindingFlags.Static | BindingFlags.Public);     
+				if (method == null)           
+					Debug.LogError("Can't find method!");     
+			
+				method.Invoke(null, null);    
+			}    
+			catch (Exception ex)    
+			{     
+				Debug.LogError("Injection failed!, Error: " + ex.Message);    
+			}
+		}
+		
+		//...
+	}
+}
