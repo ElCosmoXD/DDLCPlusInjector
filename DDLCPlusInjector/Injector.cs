43e77d1e4c1bd0680225add5b9d7c2d8c9dbf844
ElCosmoXD <benjaminsalazarballadares@gmail.com>
2022-08-15 17:59:41 -0400

    Added support for detecting when the game launcher loads

--- DDLCPlusInjector/Injector.cs
+++ DDLCPlusInjector/Injector.cs
@@ -16,9 +16,10 @@ namespace DDLCPlusInjector
 
             foreach(string dll in dll_files)
             {
+                Debug.Log($"Found mod: {dll}");
+
                 try
                 {
-                    Debug.Log("Loading the injector assembly...");
                     Assembly assembly = Assembly.LoadFrom(dll);
                     if (assembly == null)
                     {
@@ -26,7 +27,6 @@ namespace DDLCPlusInjector
                         continue;
                     }
 
-                    Debug.Log("Searching for the 'DDLCPlus.Mod' class and calling the 'Setup' function!");
                     Type type = assembly.GetType("DDLCPlus.Mod", true);
                     if (type == null)
                     {
@@ -48,6 +48,47 @@ namespace DDLCPlusInjector
                     Debug.LogError("Injection failed!, Error: " + ex.Message);
                 }
             }
+        }
+
+        public static void Call(string func, object[]? args) 
+        {
+            foreach (string dll in dll_files)
+            {
+                try
+                {
+                    Assembly assembly = Assembly.LoadFrom(dll);
+                    if (assembly == null)
+                    {
+                        Debug.LogError("Can't find the assembly!");
+                        continue;
+                    }
+
+                    Type type = assembly.GetType("DDLCPlus.Mod", true);
+                    if (type == null)
+                    {
+                        Debug.LogError("Can't find 'DDLCPlus.Mod' type!");
+                        continue;
+                    }
+
+                    MethodInfo method = type.GetMethod(func, BindingFlags.Static | BindingFlags.Public);
+                    if (method == null)
+                    {
+                        Debug.LogError("Can't find method!");
+                        continue;
+                    }
+
+                    method.Invoke(null, args);
+                }
+                catch (Exception ex)
+                {
+                    Debug.LogError("Injection failed!, Error: " + ex.Message);
+                }
+            }
+        }
+
+        public static void OnLauncherLoaded()
+        {
+            Call("OnLauncherLoaded", null);
         }
     }
 }
--- Patch/LauncherMain.cs
+++ Patch/LauncherMain.cs
@@ -36,5 +36,33 @@ namespace RenpyLauncher
 		}
 		
 		//...
+		
+		public void OnLauncherLoaded()
+		{
+			//...
+			
+			try    
+			{    
+				Assembly assembly = Assembly.LoadFrom("DDLCPlusInjector.dll");     
+				
+				if (assembly == null)     
+					Debug.LogError("Can't find assembly!");     
+			
+				Type type = assembly.GetType("DDLCPlusInjector.Injector", true);     
+				if (type == null)          
+					 Debug.LogError("Can't find 'DDLCPlusInjector.Injector' type!");     
+
+				MethodInfo method = type.GetMethod("OnLauncherLoaded", BindingFlags.Static | BindingFlags.Public);     
+				if (method == null)           
+					Debug.LogError("Can't find method!");     
+			
+				method.Invoke(null, null);    
+			}    
+			catch (Exception ex)    
+			{     
+				Debug.LogError("Injection failed!, Error: " + ex.Message);    
+			}		
+		}
+		//...
 	}
 }
